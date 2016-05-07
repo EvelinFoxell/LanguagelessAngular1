806d174bc007f48fe909b3e72a5a1e4abb1377b2
Evelin Foxell <Evelin.Foxell+github@gmail.com>
2016-05-07 20:34:13 +0200

    Added an agender roll randomizer to give a random response to gender

--- js/script.js
+++ js/script.js
@@ -1,10 +1,42 @@
+function doAgenderRoll(){
+	var responses = [
+		"400 Bad Request",
+		"204 No Content",
+		"I bought shoes. How was your week?",
+		"404 File Not Found",
+		"410 Gone",
+		"417 Expectation Failed",
+		"420 Method Failure",
+		"421 Misdirected Request",
+		"424 Failed Dependency",
+		"444 No Response",
+		"499 Request has been forbidden by antivirus",
+		"No') DROP TABLE genders; --');",
+		"Why? For what?",
+		"No thanks, I'm good.",
+		"I'd rather not.",
+		"I opt out.",
+		"501 Not Implemented",
+		"503 Service Unavailable",
+		"¯\\_(ツ)_/¯",
+		"510 Not Extended",
+		"This wasn't a dating site last I checked."
+	];
+	var min = 0;
+	var max = responses.length;
+	var pseudoRndNo = Math.floor(Math.random() * (max - min + 1)) + min;
+	return responses[pseudoRndNo];
+}
+
 var app = angular
-			.module("myModule", [])
+			.module('myModule', [])
 			.controller('myController', function ($scope){
 				$scope.developer = {
-					name: "Evelin",
-					surname: "Foxell",
+					name: 'Evelin',
+					surname: 'Foxell',
 					age: new Date().getYear() - new Date(1989, 04, 28).getYear(),
-					github: "https://github.com/EvelinFoxell"
+					gender: doAgenderRoll(),
+					github: 'https://github.com/EvelinFoxell'
 				};
+				$scope.agenderRoll = doAgenderRoll();
 			});
